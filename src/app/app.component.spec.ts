d148e5daed429d9d730e86fd28a5068783c19538
Kharraz-Mohamed-El-Amine <kharraz.mohamed@ensi.ma>
2026-04-28 12:56:02 +0100

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterModule.forRoot([])
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-app'`, () => {
+  it(`should have the 'test11' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-app');
+    expect(app.title).toEqual('test11');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('C est un composant parent');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { FilsComponent } from './fils/fils.component';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet, FormsModule, RouterLink],
   templateUrl: './app.component.html',
-  standalone: false,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'test11';
+  valparent:string = ''
+  valf:String = ''
+  valeur2:string='binding 2 directions'
+  eventCounter=1
+  TwoWayInput:any
+  valeurDfils(event:any){
+     this.valf=event
+  }
+  method1(){
+    return 2;
+  }
+  isAdmin=false;
+  increment(){
+    this.eventCounter ++;
+  }
+  decrement(){
+    this.eventCounter --;
+  }
 }
